4b26886fa66a7e112b538e1320143c9b2d586717
Srijan1214 <srijan1214@gmail.com>
2020-12-08 15:32:03 -0500

    Wrote documentation for Human

--- app/src/main/java/com/example/pinochleopl/Human.java
+++ app/src/main/java/com/example/pinochleopl/Human.java
@@ -14,6 +14,17 @@ import android.util.Pair;
 import java.util.ArrayList;
 
 public class Human extends Player {
+    /* *********************************************************************
+    Function Name: get_meld_help_message 
+    Purpose: To get the help message and the recommended meld card indexes to meld.
+    Parameters:
+    			None
+    Return Value: A pair object of String, and ArrayList<Integer>. The first is the help message and 
+					the second is the list of card indexes recommended to play for meld.
+    Local Variables:
+				None
+    Assistance Received: none
+    ********************************************************************* */
     public Pair<String, ArrayList<Integer>> get_meld_help_message() {
         Pair<ArrayList<Integer>, Integer> recommended_meld = this.get_card_ids_and_meld_number_12_best_meld();
         ArrayList<Integer> meld_card_ids = (ArrayList<Integer>) recommended_meld.first.clone();
@@ -39,6 +50,18 @@ public class Human extends Player {
         }
     }
 
+    /* *********************************************************************
+    Function Name: get_card_to_throw_help_message 
+    Purpose: To get the help message and the recommended meld card index to throw.
+    Parameters:
+    			lead_card, an integer, that tells the card if of the lead card. It is -1 if we are the lead
+				player.
+    Return Value: A pair object of String, and ArrayList<Integer>. The first is the help message and 
+					the second is a list of size 1 that tells the recommended card index to throw..
+    Local Variables:
+				None
+    Assistance Received: none
+    ********************************************************************* */
     public Pair<String, ArrayList<Integer>> get_card_to_throw_help_message(int lead_card) {
         int index;
         String message = "Recommend you play ";
@@ -75,6 +98,18 @@ public class Human extends Player {
         return new Pair<String, ArrayList<Integer>>(message, ret_list);
     }
 
+    /* *********************************************************************
+    Function Name: perform_meld_if_valid
+    Purpose: To perform the meld if the card indexes arguments allows for a meld.
+    Parameters:
+    			card_indexes, an ArrayList of integers, that tell the indexes of the cards the user
+				wants to use for a meld
+    Return Value: A pair object of String, and ArrayList<Integer>. The first is the help message and 
+					the second is a list of size 1 that tells the recommended card index to throw..
+    Local Variables:
+				None
+    Assistance Received: none
+    ********************************************************************* */
     public int perform_meld_if_valid(ArrayList<Integer> card_indexes) {
         ArrayList<Integer> meld_cards = this.get_ids_from_indexes(card_indexes);
 
